17e2d6c23e3a469945fa8428b55c730e849b8de7
jxie007 <jxie007@e.ntu.edu.sg>
2015-09-14 17:17:16 +0800

    4 methods to pass data to view

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class PagesController extends Controller
+{
+    public function about()
+    {
+    	//first method: $name = "Jeffery Way";
+    	//return view('pages.about')->with('name', $name);
+
+    	//second method: : first is key and Jeffery is the value 
+    	// turn key into variable and value to the value of the variable
+    	// return view('pages.about')->with([
+    	// 	'first' => 'Jeffery',
+    	// 	'last' => 'Way'
+    	// ]);
+
+    	//third method:
+    	// $data = [];
+    	// $data['first'] = 'Xie';
+    	// $data['last'] = 'Jun';
+    	// return view('pages.about', $data);
+
+    	//Forth method:
+    	$first = 'Xie';
+    	$last = 'Jun';
+    	return view('pages.about', compact('first', 'last'));
+    }
+}
